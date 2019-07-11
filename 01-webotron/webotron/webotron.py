b59d15af50110b56ec2c980d73eb10f5f4424cbe
Wollermann <34587@nelnet.net>
2019-07-11 16:14:47 -0500

    Clean up code style per pycodestyle, pydocstyle, and pylint

--- 01-webotron/webotron/webotron.py
+++ 01-webotron/webotron/webotron.py
@@ -1,48 +1,71 @@
-import boto3
-import click
-import re
-from botocore.exceptions import ClientError
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+"""Webotron: Deploy websites to AWS.
+
+Webotron automates the process of deploying static websites to AWS.
+- Configure AWS S3 list_buckets
+    - Create them
+    - Set them up for static website hosting
+    - Deploy local files to them
+- Configure DNS with AWS Route 53
+- Configurea a Conent Delivery Network with SSL and CloudFront
+"""
+
 from pathlib import Path
 import mimetypes
+import re
+
+import boto3
+from botocore.exceptions import ClientError
+import click
+
+
 
 session = boto3.Session(profile_name='pythonAutomation')
 s3 = session.resource('s3')
 
+
 @click.group()
 def cli():
-    "Webotron deploys websites to AWS"
+    """Webotron deploys websites to AWS."""
     pass
+
 
 @cli.command('list-buckets')
 def list_buckets():
-    "List all s3 buckets"
+    """List all s3 buckets."""
     regex = re.compile("automating")
     for bucket in s3.buckets.all():
         if regex.search(bucket.name):  # If we find a match do the following:
             print(bucket)
 
+
 @cli.command('list-bucket-objects')
 @click.argument('bucket')
 def list_bucket_objects(bucket):
-    "List objects in an S3 bucket"
+    """List objects in an S3 bucket."""
     for obj in s3.Bucket(bucket).objects.all():
-        print (obj)
+        print(obj)
+
 
 @cli.command('setup-bucket')
 @click.argument('bucket')
 def setup_bucket(bucket):
-    "Create and configure S3 bucket"
+    """Create and configure S3 bucket."""
     s3_bucket = None
     try:
         s3_bucket = s3.create_bucket(
             Bucket=bucket,
-            CreateBucketConfiguration={'LocationConstraint': session.region_name}
+            CreateBucketConfiguration={
+                'LocationConstraint': session.region_name
+                }
         )
-    except ClientError as e:
-        if e.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
+    except ClientError as error:
+        if error.response['Error']['Code'] == 'BucketAlreadyOwnedByYou':
             s3_bucket = s3.Bucket(bucket)
         else:
-            raise e
+            raise error
 
     policy = """
     {
@@ -63,8 +86,9 @@ def setup_bucket(bucket):
     pol = s3_bucket.Policy()
     pol.put(Policy=policy)
 
-    ws = s3_bucket.Website()
-    ws.put(WebsiteConfiguration={
+    # ws = s3_bucket.Website()
+    # ws.put(WebsiteConfiguration={
+    s3_bucket.Website().put(WebsiteConfiguration={
         'ErrorDocument': {
             'Key': 'error.html'
         },
@@ -75,20 +99,23 @@ def setup_bucket(bucket):
 
     return
 
+
 def upload_file(s3_bucket, path, key):
+    """Upload path to s3_bucket at key."""
     content_type = mimetypes.guess_type(key)[0] or 'text/plain'
     s3_bucket.upload_file(
         path,
         key,
         ExtraArgs={
-            'ContentType': 'text/html'
+            'ContentType': content_type
         })
+
 
 @cli.command('sync')
 @click.argument('pathname', type=click.Path(exists=True))
 @click.argument('bucket')
-def sync (pathname, bucket):
-    "Sync contents of PATHNAME to BUCKET"
+def sync(pathname, bucket):
+    """Sync contents of PATHNAME to BUCKET."""
     s3_bucket = s3.Bucket(bucket)
 
     root = Path(pathname).expanduser().resolve()
@@ -98,9 +125,13 @@ def sync (pathname, bucket):
             if p.is_dir():
                 handle_directory(p)
             if p.is_file():
-                #print("Path: {}\n Key: {}".format(p, p.relative_to(root)))
-                #upload_file(s3_bucket, str(p), str(p.relative_to(root)).replace("\\","/"))
-                upload_file(s3_bucket, str(p), (p.relative_to(root)).as_posix())
+                # print("Path: {}\n Key: {}".format(p, p.relative_to(root)))
+                # upload_file(
+                #   s3_bucket, str(p),
+                #   str(p.relative_to(root)).replace("\\","/"))
+                upload_file(
+                    s3_bucket, str(p),
+                    (p.relative_to(root)).as_posix())
 
     handle_directory(root)
 
